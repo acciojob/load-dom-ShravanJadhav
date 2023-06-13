df071e7821eb2999601722860b9b2d7a441fd2f4
Shravan Jadhav <96675283+ShravanJadhav@users.noreply.github.com>
2023-06-13 21:31:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //your JS code here. If required.
-let h1 = document.createElement("h1");
+
+
+document.addEventListener("loadDom", function(){
+	let h1 = document.createElement("h1");
 h1.innerHtml="DOM load success";
 document.body.append(h1);
+})
