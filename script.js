885fd6275e81b6d6a4f36d864bc3ff493c786a71
Shravan Jadhav <96675283+ShravanJadhav@users.noreply.github.com>
2023-06-20 17:20:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //your JS code here. If required.
 
-
-document.addEventListener("loadDom", function(){
-	 let body = document.querySelector("body");
-body.innerHTML = "";
-	var message = document.createTextNode("DOM load success");
- body.appendChild(message);
+document.addEventListener('DOMContentLoaded', function() {
+  var body = document.querySelector('body');
+  body.textContent = 'DOM load success';
 });
+
+// document.addEventListener("loadDom", function(){
+// 	 let body = document.querySelector("body");
+// body.innerHTML = "";
+// 	var message = document.createTextNode("DOM load success");
+//  body.appendChild(message);
+// });
